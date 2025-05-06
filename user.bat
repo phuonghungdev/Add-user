be851a0111aba3a4ebc75ef30addf28d5738d8fc
nguyenphuonghung <147066838+phuonghungdev@users.noreply.github.com>
2025-05-07 01:23:15 +0700

    Update user.bat

--- user.bat
+++ user.bat
@@ -1,42 +1,42 @@
 @echo off
-:: Doi ma ky tu sang UTF-8
 chcp 65001 >nul
 
-:: Kiem tra quyen administrator
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo.
     echo [LOI] Hay chay file nay bang quyen Administrator.
-    echo Click chuot phai > "Run as administrator"
     pause
     exit /b
 )
 
 setlocal
-
-:: Thong tin tai khoan
 set "USERNAME=Test"
-set "PASSWORD=T3st@User2025#"
+set "PASSWORD=Admin@123123#"
 
 :: Tao user
 net user %USERNAME% %PASSWORD% /add
 
-:: Goi nhom Administrators (neu co)
+:: Bo khoi nhom Administrators (neu co)
 net localgroup Administrators %USERNAME% /delete 2>nul
 
-:: Them vao nhom Users
+:: Them vao nhom Users va Remote Desktop Users
 net localgroup Users %USERNAME% /add
-
-:: Cho phep Remote Desktop
 net localgroup "Remote Desktop Users" %USERNAME% /add
 
-:: Su dung PowerShell de dat tuy chon mat khau (an toan hon WMIC)
+:: Cai dat quyen khong duoc doi mat khau, khong het han mat khau
 powershell -Command "Set-LocalUser -Name '%USERNAME%' -PasswordNeverExpires \$true -UserMayChangePassword \$false"
+
+:: Chặn quyền tắt máy
+reg add "HKU\%USERNAME%\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoShutDown /t REG_DWORD /d 1 /f
+
+:: Chặn quyền khởi động lại máy
+reg add "HKU\%USERNAME%\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoRestart /t REG_DWORD /d 1 /f
 
 echo.
 echo === USER DA TAO THANH CONG ===
 echo User: %USERNAME%
-echo Da co quyen dang nhap Remote Desktop.
+echo Co the dang nhap Remote Desktop.
 echo Khi cai app se bi hoi mat khau admin.
+echo User da bi chan quyen tat may va khoi dong lai may.
 echo.
 pause
